9b55b8d3d05477cfaf29890a04500fffb705e65f
David Kahana <david.kahana@gmail.com>
2017-06-23 15:20:16 +0300

    started json work

--- JsonSettings.cpp
+++ JsonSettings.cpp
@@ -14,10 +14,32 @@ void CJsonSettings::createDefaultConfig()
 	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
 	Document d;
 	d.Parse(json);
+	 
+	d.AddMember("Name", "david k.", d.GetAllocator());
+	// 2. Modify it by DOM.
+	Value& s = d["stars"];
+	s.SetInt(s.GetInt() + 1);
 
 	StringBuffer buffer;
 	Writer<StringBuffer> writer(buffer);
 	d.Accept(writer);
 
 	printf_s("%s\n", buffer.GetString());
+	//enum Type {
+	//	kNullType = 0,      //!< null
+	//	kFalseType = 1,     //!< false
+	//	kTrueType = 2,      //!< true
+	//	kObjectType = 3,    //!< object
+	//	kArrayType = 4,     //!< array 
+	//	kStringType = 5,    //!< string
+	//	kNumberType = 6     //!< number
+	//};
+	for (Value::ConstMemberIterator itr = d.MemberBegin();
+		itr != d.MemberEnd(); ++itr)
+	{
+		printf_s("Type of member %s is %d\n",itr->name.GetString(), (int)itr->value.GetType());
+	}
+	Value::ConstMemberIterator itr = d.FindMember("Name");
+	if (itr != d.MemberEnd())
+		printf_s("%s\n", itr->value.GetString());
 }
